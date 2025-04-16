68ba1e04e8718d7873ec09739b68dd7fbe0cb009
trung <trungbk.31415@gmail.com>
2025-04-16 22:53:16 +0700

    feat(be): add unit tests for student repo

---
+++ backend/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src/', '<rootDir>/tests/'],
+    testMatch: ['**/*.test.ts', '**/*.spec.ts'],
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+};
